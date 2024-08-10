2f9c05b71694f76faca83a9390b865f7ce2a46de
Michael Smith <mike@goswift.ly>
2024-08-11 00:49:50 +0100

    Took out jump_to_edit_params_screen()

--- lib/parameters.lua
+++ lib/parameters.lua
@@ -1,5 +1,9 @@
 -- For managing the parameters that a user can set
 -- Documentation at https://monome.org/docs/norns/reference/params
+function group_changed()
+  print("group_changed()")
+end
+
 
 function parameters_init()
   print("In params_init()")
@@ -11,7 +15,8 @@ function parameters_init()
 
   params:add_separator("idSep1", "Taweet Parameters")
   params:add_option("groups","Group:", groups, 1) -- FIXME should set to current group
-
+  params:set_action(params.lookup["groups"], group_changed) -- FIXME
+  
   params:add_number("something1", "something1", 20, 240,88)
   params:add_text("idText2", "", "some text for num 1")
   params:add_number("tempo", "tempo", 20, 240,88)
@@ -25,26 +30,3 @@ function parameters_init()
   clock.add_params()
 end
 
-
--- Jumps from the application screen to the script's Params Edit screen so that user can 
--- easily change app params. For when k1 pressed from within the application. Really nice
--- feature since it makes param changes easier.
-function jump_to_edit_params_screen()
-  -- Change to menu mode 
-  _menu.set_mode(true) 
-
-  -- Get access to the PARAMS menu class
-  params_class = require "core/menu/params"
-
-  -- Go to EDIT screen of the PARAMS menu. Needed in case user was at another PARAMS screen, like PSET
-  params_class.mode = 1 -- mEDIT screen
-    
-  -- Set to first settable item, which will be Group:
-  params_class.pos = 1
-  
-  -- Change to PARAMS menu screen
-  _menu.set_page("PARAMS")
-
-  -- Initialize the params page
-  params_class.init()
-end
