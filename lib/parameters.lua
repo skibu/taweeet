ce72bd5f2635ae58a72fb0aaa36bcf7975383866
Michael Smith <mike@goswift.ly>
2024-08-27 19:49:53 +0100

    many improvements

--- lib/parameters.lua
+++ lib/parameters.lua
@@ -9,8 +9,8 @@ local group_timer = nil
 
 -- Called when user stops changing the group selector for a bit
 local function group_timer_expired()
-  group_selected = params:string("groups")
-  species_list = getSpeciesForGroup(group_selected)
+  local group_selected = params:string("groups")
+  local species_list = getSpeciesForGroup(group_selected)
 
   -- Update the species selector
   local species_param = params:lookup_param("species")
@@ -60,13 +60,31 @@ end
 
 -- Called when user selects an image file for the species. Loads and shows that file.
 local function image_changed_by_encoder(index)
+  -- If haven't been initialized yet don't need to do anything
+  if global_species_data == nil then return end
+  
   util.debug_tprint("image_changed_by_encoder() index="..index)
+
+  local species_name = global_species_data.speciesName
+  local image_url = global_species_data.imageDataList[index].imageUrl
+  util.debug_tprint("Selected image url="..image_url)
+
+  select_png(image_url, species_name)
 end
 
 
 -- Called when user selects an audio file for the species. Loads and plays that file.
 local function audio_changed_by_encoder(index)
+  -- If haven't been initialized yet don't need to do anything
+  if global_species_data == nil then return end
+  
   util.debug_tprint("audio_changed_by_encoder() index="..index)
+  
+  local species_name = global_species_data.speciesName
+  local wav_url = global_species_data.audioDataList[index].audioUrl
+  util.debug_tprint("Selected audio url="..wav_url)
+
+  select_wav(wav_url, species_name)
 end
 
 
@@ -113,32 +131,11 @@ function update_parameters_for_new_species(species_data)
       -- If should use catalog
       --title = "+"..image_data.rating.." Ebird "..image_data.catalog
       
-      -- If should use location. 
-      -- Shorten the country name. List of codes is at https://www.iban.com/country-codes .
-      -- State ones at https://www.faa.gov/air_traffic/publications/atpubs/cnt_html/appendix_a.html
-      title = "+"..image_data.rating.." "..
-        image_data.loc:gsub("United States", "USA")
-                      :gsub("California", "CA")
-                      :gsub("Colorado", "CO")
-                      :gsub("Florida", "FL")
-                      :gsub("Idaho", "ID")
-                      :gsub("Indiana", "IN") -- Needs to be before "India"
-                      :gsub("Louisiana", "LA")
-                      :gsub("Mississippi", "MS")
-                      :gsub("North Carolina", "NC")
-                      :gsub("Pennsylvania", "PA")
-                      :gsub("Texas", "TX")
-                      :gsub("Washington", "WA")
-                      
-                      :gsub("Brazil", "BRA")
-                      
-                      :gsub("Canada", "CAN")
-                      :gsub("Alberta", "AB")
-                      :gsub("Quebec", "QC")
-                      
-                      :gsub("Germany", "DEU")
-                      :gsub("India", "IND")
-                      :gsub("Thailand", "THA")
+      -- If should use location. Don't need rating for images since the first
+      -- 10 will be +5.
+      title = i..")"..image_data.loc
+        :gsub("Abbreviate this", "ABV")
+        :gsub(", British Columbia", ", BC")
     end
     table.insert(images_list, title)
   end
@@ -148,14 +145,46 @@ function update_parameters_for_new_species(species_data)
   images_param.selected = 1 -- FIXME
   
   -- Update the audio selector
-  --FIXME
+  local audio_list = {}
+  for i, audio_data in ipairs(species_data.audioDataList) do
+    -- Determine title to use. If title specified in the JSON data then use it
+    local title
+    if audio_data.title then
+      title = audio_data.title
+    else
+      -- If should use catalog
+      --title = "+"..audio_data.rating.." Ebird "..audio_data.catalog
+      
+      -- If should use location. Prefix with rating since there are some species
+      -- where there aren't very many recordings, and some of them might not be that great.
+      title = i..")".."+"..audio_data.rating.." "..audio_data.loc
+      :gsub("Abbreviate this", "ABV")
+      :gsub(", British Columbia", ", BC")
+    end
+    table.insert(audio_list, title)
+  end
+  local audio_param = params:lookup_param("audio")
+  audio_param.options = audio_list
+  audio_param.count = #audio_list
+  audio_param.selected = 1 -- FIXME
 end
 
+-- For shortening the label strings of parameters so that the value doesn't
+-- overlap with the label. Just removing spaces after comma for a slight 
+-- improvement.
+local function shortener_function(value)
+  local result = value:gsub(", ", ",")
+  return result
+end  
 
 -- Initializes the parameters for the Taweeet application.
 -- To be called from the applications main init() function.
 function parameters_init()
   print("Initializing parameters...")
+  
+  -- To help eliminate overlap of parameter values with their labels
+  util.debug_tprint("XXXXXX setting shortener")
+  selector_shortener(shortener_function)
   
   -- get rid of standard params so that they are not at the top
   params:clear()
@@ -165,7 +194,7 @@ function parameters_init()
 
   -- Group timer used to not call group_changed() callback until after encoder
   -- has stopped moving for a bit. This greatly reduces the number of callbacks
-  group_timer = metro.init(group_timer_expired, 1.0, 1)
+  group_timer = metro.init(group_timer_expired, 0.7, 1)
 
   -- Group selector. Since groups_list doesn't change, the selector can be 
   -- fully created
@@ -175,7 +204,7 @@ function parameters_init()
   
   -- Species timer used to not call species_changed() callback until after encoder
   -- has stopped moving for a bit. This greatly reduces the number of callbacks
-  species_timer = metro.init(species_timer_expired, 1.0, 1)
+  species_timer = metro.init(species_timer_expired, 0.7, 1)
 
   -- Species selector. Since group not yet selected, cannot set the species list 
   -- for the group yet. Therefore just using empty list for now.
@@ -191,12 +220,12 @@ function parameters_init()
   params:set_action(params.lookup["audio"], audio_changed_by_encoder)
   
   -- Adding some other params just to play around
+  params:add_text("", "") -- A spacer
   params:add_separator("test params, for fun")
   params:add_number("something1", "something1", 20, 240,88)
   params:add_number("tempo", "tempo", 20, 240,88)
 
   -- Add back the standard params, but so that they are at the end of the page
-  params:add_text("", "")
   params:add_separator("Standard Parameters")
   -- add back standard audio params like LEVELS, REVERB, COMPRESSOR, and SOFTCUT
   audio.add_params()
