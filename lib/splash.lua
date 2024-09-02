d350711d95ac229bff4549dfce9b4cffecee31ee
Michael Smith <mike@goswift.ly>
2024-09-02 04:09:17 +0100

    renamed some functions

--- lib/splash.lua
+++ lib/splash.lua
@@ -17,12 +17,12 @@ local display_time = 0
 -- for when switching back to app window. 
 -- min_time: minimum time in seconds that splash screen should be displayed for
 -- returns: true if splash screen displayed
-function displaySpashScreenOnceViaRedraw(min_time)
+function display_splash_screen_once_via_redraw(min_time)
   -- If already displayed via redraw then don't need to do it again
   if splash_screen_displayed_via_redraw then return false end
 
   -- If want to make sure splash screen displayed for minimum amount of time.
-  -- Need to determine sleep_time before displaySplashScreen() is called since
+  -- Need to determine sleep_time before display_splash_screen() is called since
   -- that will overwrite the display_time.
   local sleep_time = 0
   if min_time ~= nil then
@@ -37,7 +37,7 @@ function displaySpashScreenOnceViaRedraw(min_time)
   -- then display it and sleep
   if sleep_time > 0 then  
     -- Need to display it
-    displaySplashScreen()
+    display_splash_screen()
     
     -- Now that splash screen displayed again, sleep so that it is up desired amount of time
     util.debug_tprint("Sleeping ".. string.format("%.2f", sleep_time)..
@@ -50,10 +50,14 @@ end
   
 -- Can be called directly from init() to display splash screen before get to redraw().
 -- This way splash screen is displayed immediately.
-function displaySplashScreen()
+function display_splash_screen()
+  -- Splash image is from git so it is in the app's code directory
+  local filename = norns.state.path .. "images/splash.png"
+  local width, height = screen.extents(filename)
+  
   -- Display splash image
   screen.clear()
-  screen.display_png("/home/we/dust/code/taweeet/images/splash.png", 0, 0)
+  screen.display_png(filename, (128-width)/2, (64-height)/2)
   screen.update()
   
   -- Remember when first displayed in case want to make sure it is displayed
