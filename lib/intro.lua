803e0ca4993bb5967beb02977eb9ec296759a6da
Michael Smith <mike@goswift.ly>
2024-09-02 04:10:20 +0100

    Big change to intro animation Now instead of just boring scrolling species image from left to right, it is instead split into four quadrants and swirled around the center of the screen. Kind of cool what one can do with image buffers.

--- lib/intro.lua
+++ lib/intro.lua
@@ -1,52 +1,45 @@
 -- Displays the graphical intro for the selected species
 
 
--- Animates the intro by updating the global current_count while calling
--- displayIntroForSpeciesCallback repeatedly
-function introTick(count)
-  displayIntroForSpeciesCallback(count)
-end
+-- Parameters used to affect the intro animation
+local screen_width = 128     -- Standard norns screen width
+local screen_height = 64     -- Standard norns screen height
+local initial_radius = 50.   -- Number of pixels animation should start from center of screen
+local animation_ticks = 70   -- How many ticks for the swirling animation of the image
+local rectangle_y_pause = 58 -- Where species name text rectangle should pause
+local pause_ticks = 30       -- How long to pause there
 
-
--- Timer for doing intro animation. To start the animation call
--- intro_clock:start()
-local intro_clock = metro.init(introTick, 0.05, -1)
-
-
--- Initiates the display of the visual introduction for the species, 
--- but only does so if the PNG is ready. To be called by redraw(). If PNG
--- not yet ready then the intro will be initiated by the completino of
--- getting the PNG in png_file_exists_callback() in species.lua
-function startIntro()
-  if png_ready() then
-    util.debug_tprint("startIntro() Starting intro since png now ready")
-    intro_clock:start()
+-- Returns x,y coordinatess of the corner of the intro image to be displayed.
+-- The current_count specifies how far along the animation should be, and 
+-- should come from the intro clock. The offset_degrees parameter is so can
+-- use use this function for all four portions of the intro image to be displayed.
+-- 0 for x axis, 90 for y axis, 180 for -x, and 270 for -y. The x,y location 
+-- returned is for screen coordinates where, confusingly, y increases going downwards.
+local function get_x_y(current_count, offset_degrees)
+  -- If done with animation then just return center of screen
+  if current_count > animation_ticks then
+    return screen_width/2, screen_height/2
   end
+  
+  -- Decreasing radius and increasing angle
+  radius = initial_radius * (1 - current_count/animation_ticks)
+  angle_degrees = offset_degrees + (current_count / (animation_ticks/2)) * 360
+  angle_rads = math.rad(angle_degrees)
+  
+  -- For the axis vector (x, y, -x, -y) determinined by offset_degrees
+  x = screen_width/2 + radius * math.cos(angle_rads)
+  y = screen_height/2 - radius * math.sin(angle_rads)
+  
+  return x, y
 end
 
 
--- Initiates the display of the visual introduction for the species, 
--- but only does so if in app mode instead of menu mode. To be called
--- by the callback when PNG fully loaded. If in menu mode then the
--- intro will be initiated by redraw()
-function startIntroIfInAppMode()
-  if not _menu.mode then
-    util.debug_tprint("startIntroIfInAppMode Starting intro since in app mode instead of menu mode")
-    intro_clock:start()
-  end
-end
-
-
--- Stops the intro counter. Useful for when jumping to parameter menu.
--- Okay to call even if intro not currently running.
-function haltIntro()
-  util.debug_tprint("Halting intro in intro.haltIntro()")
-  intro_clock:stop()
-end
-
-
-function displayNewIntroCallback(current_count)
-  --util.debug_tprint("Redrawing via displayIntroForSpeciesCallback()")
+-- Called every clock click for animating introduction. Displays the image of the 
+-- current species by dividing the image into 4 sequares and swirling them around
+-- the center of the screen until they are full sized and in proper place. Also
+-- scrolls from top to bottom the text of the name of the current species.
+local function swirling_intro_callback(current_count)
+  --util.debug_tprint("Redrawing via swirling_intro_callback()")
   
   -- To make sure that no longer have problem when hit k2 twice, the second time 
   -- before the first intro finished
@@ -57,12 +50,32 @@ function displayNewIntroCallback(current_count)
   
     -- Always start by clearing screen
   screen.clear()
+
+  local image_width = global_species_data.width
+  local image_height = global_species_data.height
+  local image_buffer = global_species_data.image_buffer
   
-  -- FIXME the graphics for the new intro go here
-  --
-  --
+  width = (image_width/2) * math.min(current_count, animation_ticks)/animation_ticks
+  height = (image_height/2) * math.min(current_count, animation_ticks)/animation_ticks
   
-  
+  -- top left, tied to left axis
+  x, y = get_x_y(current_count, 180.0)
+  screen.display_image_region (image_buffer, 0, 0, width, height, x-width, y-height)
+
+  -- bottom left, tied to down axis
+  x, y = get_x_y(current_count, 270.0)
+  --screen.display_image_region (test_buffer, 0, 32, 61, 32, 3, 32)
+  --screen.display_image_region (image, left, top, width, height, x, y)
+  screen.display_image_region (image_buffer, 0, image_height-height, width, height, x-width, y)
+
+  -- bottom right, tied to right axis
+  x, y = get_x_y(current_count, 0.0)
+  screen.display_image_region(image_buffer, image_width-width, image_height-height, width, height, x, y)
+
+  -- top right, tied to right axis
+  x, y = get_x_y(current_count, 90.0)
+  screen.display_image_region(image_buffer, image_width-width, 0, width, height, x, y-height)
+
   -- Draw some vertically moving name of the species.
   -- First draw a darker rectangle so text will be readable. And to
   -- do that first need to figure out proper font size for the species name.
@@ -79,8 +92,6 @@ function displayNewIntroCallback(current_count)
   local rectangle_x = (128 - text_width - 2*horiz_padding) / 2
   
   -- Since moving downwards but pausing at certain height, rectanble_y is more difficult
-  local rectangle_y_pause = 58 -- Where rectangle should pause
-  local pause_ticks = 60       -- How long to pause there
   local rectangle_y = current_count - font_size - 1
   if current_count > rectangle_y_pause and current_count <= rectangle_y_pause + pause_ticks then
     rectangle_y = rectangle_y_pause - font_size - 1
@@ -102,24 +113,21 @@ function displayNewIntroCallback(current_count)
   -- update so that drawing actually visible
   screen.update()
   
-  --util.debug_tprint("Done with displayIntroForSpeciesCallback()")
+  --util.debug_tprint("Done with swirling_intro_callback()")
   
-  if rectangle_y > 64 then
-    util.debug_tprint("Stopping intro clock because done with animation")
+  -- Done if text rectangle disappeared and done with image animation
+  if rectangle_y > 64 and current_count >= animation_ticks and intro_clock ~= nil then
+    util.debug_tprint("Stopping intro clock because done with animation current_count="..current_count.." and animation_ticks="..animation_ticks)
     intro_clock:stop()
   end
-  
-  -- FIXME only returning true for developing this intro
-  return true
 end
 
 
--- Called every clock tick when displaying the visual intro for the species
-function displayIntroForSpeciesCallback(current_count)
-  -- FIXME just for developing new, fancier intro
-  if displayNewIntroCallback(current_count) then return end
-  
-  --util.debug_tprint("Redrawing via displayIntroForSpeciesCallback()")
+-- Deprecated.
+-- The original animation which just scrolls the image from left to right.
+-- To be called every clock tick when displaying the visual intro for the species.
+local function original_scroll_image_intro_callback(current_count)
+  util.debug_tprint("Redrawing via original_scroll_image_intro_callback()")
   
   -- To make sure that no longer have problem when hit k2 twice, the second time 
   -- before the first intro finished
@@ -139,7 +147,6 @@ function displayIntroForSpeciesCallback(current_count)
   local png_x = -png_width + 6*current_count
   if png_x > (128-png_width)/2 then 
     -- Reached desired horizontal position so don't increase png_x anymore
-    --if debug_mode then print("png centered!") end
     png_x = (128-png_width)/2
   end
   
@@ -185,11 +192,55 @@ function displayIntroForSpeciesCallback(current_count)
   -- update so that drawing actually visible
   screen.update()
   
-  --util.debug_tprint("Done with displayIntroForSpeciesCallback()")
+  util.debug_tprint("Done with original_scroll_image_intro_callback()")
   
   if rectangle_y > 64 then
-    util.debug_tprint("Stopping intro clock because done with animation")
+    util.debug_tprint("Stopping intro clock because done with animation count="..current_count)
     intro_clock:stop()
   end
+end
+
+
+-- Animates the intro by updating the global current_count while calling
+-- swirling_intro_callback repeatedly
+function intro_tick(count)
+  swirling_intro_callback(count)
+end
+
+
+-- Timer for doing intro animation. The animation intro clock runs until it is
+-- stopped. To start the animation call intro_clock:start()
+local intro_clock = metro.init(intro_tick, 0.04, -1)
+
+
+-- Initiates the display of the visual introduction for the species, 
+-- but only does so if the PNG is ready. To be called by redraw(). If PNG
+-- not yet ready then the intro will be initiated by the completino of
+-- getting the PNG in png_file_exists_callback() in species.lua
+function startIntro()
+  if png_ready() then
+    util.debug_tprint("startIntro() Starting intro since png now ready")
+    intro_clock:start()
+  end
+end
+
+
+-- Initiates the display of the visual introduction for the species, 
+-- but only does so if in app mode instead of menu mode. To be called
+-- by the callback when PNG fully loaded. If in menu mode then the
+-- intro will be initiated by redraw()
+function startIntroIfInAppMode()
+  if not _menu.mode then
+    util.debug_tprint("startIntroIfInAppMode() Starting intro since in app mode, not menu mode")
+    intro_clock:start()
+  end
+end
+
+
+-- Stops the intro counter. Useful for when jumping to parameter menu.
+-- Okay to call even if intro not currently running.
+function haltIntro()
+  util.debug_tprint("Halting intro in intro.haltIntro()")
+  intro_clock:stop()
 end
 
