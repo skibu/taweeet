553ff853b26e85c137328e4be143358a06e606fd
Michael Smith <mike@goswift.ly>
2024-09-29 05:09:40 +0100

    Refactored so can make additional changes
    Just checked it in because it is working and going to make more changes.

--- lib/intro.lua
+++ lib/intro.lua
@@ -8,26 +8,33 @@ local initial_radius = 50.   -- Number of pixels animation should start from cen
 local animation_ticks = 70   -- How many ticks for the swirling animation of the image
 local rectangle_y_pause = 58 -- Where species name text rectangle should pause
 local pause_ticks = 30       -- How long to pause there
+local mask_extra_width = 20. -- extra width so can make binoculars pan a bit horizontally
 
 
--- Draws mask. To be called via screen.draw_to() so that the drawing will occur
--- on the specified image. The image is assumed to be full screen, which is
+-- Timer for doing intro animation. Once started, the animation intro clock runs until
+-- it is stopped. To start the animation call intro_clock:start(). The clock is initted
+-- lower down in this file.
+local intro_clock
+
+
+-- Draws mask onto image buffer. To be called via screen.draw_to() so that the drawing
+-- will occur on the specified image. The image is assumed to be full screen, which is
 -- 128x64.
 local function draw_mask(dark_level) 
-  log.debug("Drawing to image using dark_level="..tostring(dark_level))
+  log.debug("Drawing mask using dark_level="..tostring(dark_level))
   
   -- Make the whole image dark
   screen.level(dark_level)  
-  screen.rect(0, 0, screen_width, screen_height)
+  screen.rect(0, 0, screen_width + mask_extra_width, screen_height)
   screen.fill()
   
   -- Draw two level 15 circles to make it sort of look like a view through binoculars
   screen.level(15)
-  local r = (screen_height / 2) - 3
-  local y = 32
-  screen.circle(32, y, r)
+  local r = (screen_height / 2) - 4
+  local y = screen_height / 2
+  screen.circle(screen_width/4 + mask_extra_width/2, y, r)
   screen.fill()
-  screen.circle(96, y, r)
+  screen.circle(3*screen_width/4 + mask_extra_width/2, y, r)
   screen.fill()
 end
 
@@ -41,9 +48,9 @@ local function create_mask_image(dark_level)
   if mask_cache ~= nil then return mask_cache end
   
   -- Create new image
-  local image = screen.create_image(screen_width, screen_height)
+  local image = screen.create_image(screen_width + mask_extra_width, screen_height)
   
-  -- Draw mask to the image using draw_mask()
+  -- Draw mask to the image buffer using draw_mask()
   screen.draw_to(image, draw_mask, dark_level)
   
   mask_cache = image
@@ -77,23 +84,9 @@ local function get_x_y(current_count, offset_degrees)
 end
 
 
--- Called every clock click for animating introduction. Displays the image of the 
--- current species by dividing the image into 4 sequares and swirling them around
--- the center of the screen until they are full sized and in proper place. Also
--- scrolls from top to bottom the text of the name of the current species.
-local function swirling_intro_callback(current_count)
-  --log.debug("Redrawing via swirling_intro_callback()")
-  
-  -- To make sure that no longer have problem when hit k2 twice, the second time 
-  -- before the first intro finished
-  if not png_ready() then
-    log.debug("Error: png not ready but tried to display intro displayIntroForSpeciesCallback()")
-    return
-  end
-  
-    -- Always start by clearing screen
-  screen.clear()
-
+-- Draw current frame of species image swirling around.
+-- Returns true when done with animation.
+local function draw_frame_of_swirling_image(current_count)
   local image_width = global_species_data.width
   local image_height = global_species_data.height
   local image_buffer = global_species_data.image_buffer
@@ -107,8 +100,6 @@ local function swirling_intro_callback(current_count)
 
   -- bottom left, tied to down axis
   x, y = get_x_y(current_count, 270.0)
-  --screen.display_image_region (test_buffer, 0, 32, 61, 32, 3, 32)
-  --screen.display_image_region (image, left, top, width, height, x, y)
   screen.display_image_region(image_buffer, 0, image_height-height, width, height, x-width, y)
 
   -- bottom right, tied to right axis
@@ -118,14 +109,26 @@ local function swirling_intro_callback(current_count)
   -- top right, tied to right axis
   x, y = get_x_y(current_count, 90.0)
   screen.display_image_region(image_buffer, image_width-width, 0, width, height, x, y-height)
+  
+  -- Return true if done with the animation
+  local done = current_count >= animation_ticks
+  return done
+end
 
-  -- Draw the maskr so that it hopefully looks as if looking through binoculars
+
+-- Draw the mask so that it (hopefully) looks as if looking through binoculars
+local function draw_binocular_mask(current_count)
   local mask = create_mask_image(0)
   screen.blend_mode("Darken")
-  screen.display_image(mask, 0, 0)
+  screen.display_image_region(mask, mask_extra_width/2, 0, screen_width, screen_height, 0, 0)
   screen.blend_mode("Over")
-  
-  -- Draw some vertically moving name of the species.
+end
+
+
+-- Draw vertically moving name of the species.
+-- Retuns true if done with animating the species name, when the
+-- display of the name is fully below the screen.
+local function draw_vertically_moving_species_name(current_count)
   -- First draw a darker rectangle so text will be readable. And to
   -- do that first need to figure out proper font size for the species name.
   screen.font_face(7)
@@ -134,7 +137,7 @@ local function swirling_intro_callback(current_count)
   repeat  
     font_size = font_size - 1
     screen.font_size(font_size)
-    text_width = screen.text_extents(global_species_data.speciesName)
+    text_width = screen.text_extents(species_name())
   until (text_width <= 128)
   
   -- rectangle_x is static and easy to determine
@@ -157,14 +160,45 @@ local function swirling_intro_callback(current_count)
   screen.level(15)
   screen.aa(1) -- Found that font 7 Roboto-Bold at large size looks better with anti-aliasing
   screen.move(rectangle_x + horiz_padding, rectangle_y - 2 + font_size)
-  screen.text(global_species_data.speciesName)
+  screen.text(species_name())
+  
+  -- Return true if done with species name animation, when it has dropped below the screen
+  local done = rectangle_y > screen_height
+  return done
+end
+
+
+-- Called every clock click for animating introduction. Displays the image of the 
+-- current species by dividing the image into 4 sequares and swirling them around
+-- the center of the screen until they are full sized and in proper place. Also
+-- scrolls from top to bottom the text of the name of the current species.
+local function swirling_intro_callback(current_count)
+  -- To make sure that no longer have problem when hit k2 twice, the second time 
+  -- before the first intro finished
+  if not png_ready() then
+    log.debug("Error: png not ready but tried to display intro displayIntroForSpeciesCallback()")
+    return
+  end
+  
+    -- Always start by clearing screen
+  screen.clear()
+
+  -- Draw frame of swirling species image
+  local done_with_animation = draw_frame_of_swirling_image(current_count)
+  
+  -- Draw the mask so that it (hopefully) looks as if looking through binoculars
+  draw_binocular_mask(current_count)
+  
+  -- Draw vertically moving name of the species.
+  local done_with_species_name = draw_vertically_moving_species_name(current_count)
   
   -- update so that drawing actually visible
   screen.update()
   
   -- Done if text rectangle disappeared and done with image animation
-  if rectangle_y > 64 and current_count >= animation_ticks and intro_clock ~= nil then
-    log.debug("Stopping intro clock because done with animation current_count="..current_count.." and animation_ticks="..animation_ticks)
+  if done_with_species_name and done_with_animation and intro_clock ~= nil then
+    log.debug("Stopping intro clock because done with all intro animation. current_count="..
+      current_count.." and animation_ticks="..animation_ticks)
     intro_clock:stop()
   end
 end
@@ -174,6 +208,7 @@ end
 -- The original animation which just scrolls the image from left to right.
 -- To be called every clock tick when displaying the visual intro for the species.
 local function original_scroll_image_intro_callback(current_count)
+  log.error("original_scroll_image_intro_callback() is deprecated!!!!!")
   log.debug("Redrawing via original_scroll_image_intro_callback()")
   
   -- To make sure that no longer have problem when hit k2 twice, the second time 
@@ -255,9 +290,9 @@ function intro_tick(count)
 end
 
 
--- Timer for doing intro animation. The animation intro clock runs until it is
--- stopped. To start the animation call intro_clock:start()
-local intro_clock = metro.init(intro_tick, 0.04, -1)
+-- Timer for doing intro animation. Once started, the animation intro clock runs until
+-- it is stopped. To start the animation call intro_clock:start()
+intro_clock = metro.init(intro_tick, 0.04, -1)
 
 
 -- Initiates the display of the visual introduction for the species, 
